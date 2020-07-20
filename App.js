3abad8ee18bcdb95c2aebb54678756c3c5c6dfa8
railsstudent <cleung334@gmail.com>
2020-07-20 22:00:14 +0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  const name = 'React';
+  return (
+    <div>
+      <p>{ `Hello ${name}` }</p>
+      <p>
+        Start editing to see some magic happen :)
+      </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-hooks-examples2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-hooks-examples2)
