92f308f5796791cb1cfe8a267ddb1e349e8b8ab0
railsstudent <cleung334@gmail.com>
2020-07-20 22:56:56 +0800

    useMemo example

--- App.js
+++ App.js
@@ -1,13 +1,10 @@
 import React from 'react';
+import Memo from './Memo';
 
 function App() {
-  const name = 'React';
   return (
     <div>
-      <p>{ `Hello ${name}` }</p>
-      <p>
-        Start editing to see some magic happen :)
-      </p>
+      <Memo />
     </div>
   );
 }
---
+++ Memo.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect, useMemo } from 'react';
+
+const fibonacci = n => {
+  if (n <= 1) {
+    return 1;
+  }
+
+  return fibonacci(n - 1) + fibonacci(n - 2);
+};
+
+const MemoComponent = () => {
+  const [num, setNum] = useState(1);
+  const [isGreen, setIsGreen] = useState(true);
+  // const fib = useMemo(() => fibonacci(num), [num]);
+  const fib = useMemo(() => fibonacci(num), [num])
+
+  const style = {
+    display: 'flex',
+    justifyContent: 'center',
+  };
+
+  return (
+    <div>
+      <h1
+        onClick={() => setIsGreen(!isGreen)}
+        style={{ color: isGreen ? "limegreen" : "crimson" }}
+      >
+        useMemo Example
+      </h1>
+      <h2>
+        Fibonacci of {num} is {fib}
+      </h2>
+      <div style={style}>
+        <button onClick={() => setNum(num + 1)}>Increment ➕</button>
+      </div>
+    </div>
+  );
+}
+
+export default MemoComponent;
